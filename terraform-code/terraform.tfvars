b5a254d8a5dfd743f737849d09d011c2a02e1492
Christopher Harris <christopherharris1995@ymail.com>
2026-02-25 16:46:34 +0000

    tf-fundamentals: lessons 28-30

--- terraform-code/terraform.tfvars
+++ terraform-code/terraform.tfvars
@@ -1,2 +1,3 @@
-repo_count = 2
-env        = "dev"
+repo_count              = 2
+env                     = "dev"
+deployment_environments = ["dev", "staging", "prod"]
--- terraform-code/variables.tf
+++ terraform-code/variables.tf
@@ -20,3 +20,18 @@ variable "env" {
     error_message = "env must be one of 'dev', 'staging', or 'prod'."
   }
 }
+
+variable "deployment_environments" {
+  type        = set(string)
+  description = "Set of environments for which to create the repositories"
+  default     = ["dev", "staging", "prod"]
+
+  validation {
+    condition     = length(var.deployment_environments) > 0
+    error_message = "Deployment environments list must contain at least one environment."
+  }
+  validation {
+    condition     = alltrue([for env in var.deployment_environments : contains(["dev", "staging", "prod"], env)])
+    error_message = "All deployment environments must be one of 'dev', 'staging', or 'prod'."
+  }
+}
