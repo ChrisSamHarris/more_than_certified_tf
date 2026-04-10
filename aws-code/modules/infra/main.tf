bbbde4afd74d9a2583dee1155ba6b2fdb18978e9
Christopher Harris <christopherharris1995@ymail.com>
2026-04-10 13:34:38 +0100

    aws-infra: import test complete

--- aws-code/modules/infra/main.tf
+++ aws-code/modules/infra/main.tf
@@ -119,3 +119,86 @@ resource "aws_iam_role_policy_attachment" "logic-iam-policy-attachment" {
   role       = aws_iam_role.logic-iam-role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+######### EC2 Import Resources #########
+
+# import {
+#   # terraform plan -generate-config-out=generated.tf -> This will generate a plan but it conflicts with ipv6 address & primary NI
+#  Identify any bugs in the generated configuration and fix them before applying the import.
+#   to = aws_instance.infra-imported-ec2-instance
+#   id = "<INSTANCE_ID>"
+# }
+# Imported 
+# 
+# Exercise for next time -> Required to import the Security Group 
+# 
+# resource "aws_instance" "infra-imported-ec2-instance" {
+#   ami                                  = "<AMI_ID>"
+#   availability_zone                    = "eu-west-2b"
+#   disable_api_stop                     = false
+#   disable_api_termination              = false
+#   ebs_optimized                        = false
+#   force_destroy                        = false
+#   get_password_data                    = false
+#   hibernation                          = false
+#   instance_initiated_shutdown_behavior = "stop"
+#   instance_type                        = "t2.micro"
+#   key_name                             = "test-instance"
+#   monitoring                           = false
+#   placement_partition_number           = 0
+#   private_ip                           = "<PRIVATE_IP>"
+#   region                               = "eu-west-2"
+#   secondary_private_ips                = []
+#   security_groups                      = ["launch-wizard-1"]
+#   source_dest_check                    = true
+#   subnet_id                            = "<SUBNET_ID>"
+#   tags = {
+#     Name = "test-import-server"
+#   }
+#   tags_all = {
+#     Name = "test-import-server"
+#   }
+#   tenancy                     = "default"
+#   user_data                   = null
+#   user_data_replace_on_change = null
+#   volume_tags                 = null
+#   vpc_security_group_ids      = ["<SECURITY_GROUP_ID>"]
+#   capacity_reservation_specification {
+#     capacity_reservation_preference = "open"
+#   }
+#   cpu_options {
+#     core_count       = 1
+#     threads_per_core = 1
+#   }
+#   credit_specification {
+#     cpu_credits = "standard"
+#   }
+#   enclave_options {
+#     enabled = false
+#   }
+#   maintenance_options {
+#     auto_recovery = "default"
+#   }
+#   metadata_options {
+#     http_endpoint               = "enabled"
+#     http_protocol_ipv6          = "disabled"
+#     http_put_response_hop_limit = 2
+#     http_tokens                 = "required"
+#     instance_metadata_tags      = "disabled"
+#   }
+#   private_dns_name_options {
+#     enable_resource_name_dns_a_record    = true
+#     enable_resource_name_dns_aaaa_record = false
+#     hostname_type                        = "ip-name"
+#   }
+#   root_block_device {
+#     delete_on_termination = true
+#     encrypted             = false
+#     iops                  = 3000
+#     tags                  = {}
+#     tags_all              = {}
+#     throughput            = 125
+#     volume_size           = 8
+#     volume_type           = "gp3"
+#   }
+# }
